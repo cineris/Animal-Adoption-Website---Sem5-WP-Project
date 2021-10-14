d4558dcc80bae8d95612be5cd71c7d1e8d0485fe
ash <ashlin.fonseca@gmail.com>
2021-10-14 17:43:02 +0530

    php for adopt

---
+++ php/adopt.php
@@ -0,0 +1,46 @@
+<?php
+    if (isset($_POST['submit'])){
+        $name = $_POST['name1'];
+        $email = $_POST['email'];
+        $number = $_POST['number'];
+        $petname = $_POST['petname'];
+    }
+
+    $dbHost = 'localhost'; //or localhost
+    $dbName = 'adoptme'; // your db_name
+    $dbUsername = 'root'; // root by default for localhost
+    $dbPassword = '';  // by default empty for localhost
+
+    $conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connected Successfully";
+
+    $sql = "CREATE TABLE if not exists adopt(
+        name VARCHAR(30) NOT NULL,
+        email_id VARCHAR(30) NOT NULL,
+        phone_number NUMERIC NOT NULL,
+        petname VARCHAR(50) NOT NULL)";
+
+    if(mysqli_query($conn, $sql))
+    {
+        echo "<br>Table created successfully</br>";
+    }
+    else
+    {
+        echo "ERROR: Could not create table " . mysqli_error($conn);
+    }
+    /*
+    $sql = "DELETE FROM adopt"; // run these two lines to clear table
+    mysqli_query($conn, $sql);
+    */
+    $sql = "INSERT INTO adopt VALUES ('$name','$email','$number','$petname')";
+    if(mysqli_query($conn, $sql)){
+        echo "Records inserted successfully.";
+    }
+    else
+    {
+        echo "ERROR: Could not insert values " . mysqli_error($conn);
+}
+ ?>
--- scripts/login_popup.js
+++ scripts/login_popup.js
@@ -1,9 +1,16 @@
 // window onload used to fix an error with adding event listeners
 window.onload =  function(){
 document.querySelector("#login-button").addEventListener("click",function(){
-  document.querySelector(".popup").classList.add("active");
+  document.querySelector(".popup.lg").classList.add("active");
 });
 document.querySelector("#close-login").addEventListener("click",function(){
-  document.querySelector(".popup").classList.remove("active");
+  document.querySelector(".popup.lg").classList.remove("active");
+});
+
+document.querySelector("#signup-button").addEventListener("click",function(){
+  document.querySelector(".popup.su").classList.add("active");
+});
+document.querySelector("#close-signup").addEventListener("click",function(){
+  document.querySelector(".popup.su").classList.remove("active");
 });
 }
